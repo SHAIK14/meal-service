b7d0166089b0a5afc8929aff69efa7175961f1f9
saqhibbilal <saqhibbilal@gmail.com>
2024-09-29 23:55:47 +0530

    changes add again

--- client/src/screens/AddressScreen.js
+++ client/src/screens/AddressScreen.js
@@ -84,7 +84,7 @@ const AddressScreen = ({ navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#e0e0e0", // Softer background color
+    backgroundColor: "yellow", // Softer background color
   },
   scrollView: {
     flexGrow: 1,
